ca49f6588011fb79a9ae6048c4b8f5ed394022da
Gabriel Gomes da Silva <gabrielgomesch@gmail.com>
2018-06-29 15:54:54 -0300

    Commit Inicial

---
+++ app/src/main/java/com/labs87/patiodecompras/CustomFirebaseHandlerInstanceIDService.java
@@ -0,0 +1,23 @@
+package com.labs87.patiodecompras;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Gabriel on 25/06/2018.
+ */
+
+public class CustomFirebaseHandlerInstanceIDService extends FirebaseInstanceIdService {
+    /*
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        //Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }*/
+}
